3693560e94de26f6bf38988e82ef11bf1655827f
pjl4 <pj.lutz17@gmail.com>
2020-02-19 12:46:05 -0500

    Updates

--- precache-manifest.8ea8ffa92cf40b97f04a5138e86c78e5.js
+++ precache-manifest.8ea8ffa92cf40b97f04a5138e86c78e5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6855fec9644e2765f3d6b459612f6993",
+    "revision": "a334cd7d8db97d4f9002cdb5bc07365f",
     "url": "/moviesFrontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/moviesFrontend/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "2227addda56aa75633bb",
+    "revision": "952c480da464edc00e24",
     "url": "/moviesFrontend/static/css/main.fa816fbc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/moviesFrontend/static/js/2.7563421f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2227addda56aa75633bb",
-    "url": "/moviesFrontend/static/js/main.e60fc875.chunk.js"
+    "revision": "952c480da464edc00e24",
+    "url": "/moviesFrontend/static/js/main.d58a9ced.chunk.js"
   },
   {
     "revision": "30604171bd805a4e1792",
