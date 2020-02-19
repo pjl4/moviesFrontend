fef39c38c4a2b2078960adf1d7251a7f923f3f51
ryansmane <ryanzimmer8@gmail.com>
2020-02-19 17:41:10 -0500

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,108 +6,104 @@ const axios = require('axios');
 const url = 'https://cinopsis.herokuapp.com/api/movies';
 
 const Home = props => {
-  const [allMovies, setAllMovies] = useState([]);
+   const [allMovies, setAllMovies] = useState([]);
 
-  //getting and setting all movies
-  useEffect(() => {
-    axios.get(url).then(res => setAllMovies(res.data));
-  }, []);
+   //getting and setting all movies
+   useEffect(() => {
+      axios.get(url).then(res => setAllMovies(res.data));
+   }, []);
 
-  const getTopFive = genre => {
-    const topFive = allMovies
-      .filter(movie => movie.genre === genre)
-      .sort((a, b) => (a.avgRating > b.avgRating ? -1 : 1));
-    topFive.splice(5, topFive.length - 1);
-    const bullets = topFive.map(movie => {
-      return (
-        <Link to={'movie/' + movie._id}>
-          <li>
-            {movie.title}{' '}
-            {movie.ratings.length > 0 && (
-              <span style={{ color: 'black' }}>
-                Average Rating:{movie.avgRating}
-              </span>
-            )}
-          </li>
-        </Link>
-      );
-    });
-    if (bullets.length >= 5) {
-      const ViewAll = addViewAll(genre);
-      bullets.push(ViewAll);
-    }
-    return bullets;
-  };
-  const addViewAll = genre => {
-    return <Link to={`genre/${genre}`}>View All</Link>;
-  };
+   const getTopFive = genre => {
+      const topFive = allMovies
+         .filter(movie => movie.genre === genre)
+         .sort((a, b) => (a.avgRating > b.avgRating ? -1 : 1));
+      topFive.splice(5, topFive.length - 1);
+      const bullets = topFive.map(movie => {
+         return (
+            <Link to={'movie/' + movie._id}>
+               <li>
+                  {movie.title}
+                  
+               </li>
+            </Link>
+         );
+      });
+      if (bullets.length >= 5) {
+         const ViewAll = addViewAll(genre);
+         bullets.push(ViewAll);
+      }
+      return bullets;
+   };
+   const addViewAll = genre => {
+      return <Link className='viewAll' to={`genre/${genre}`}>View All</Link>;
+   };
 
-  return (
-    <>
-      <main className="homeMain">
-        <div className="homeList">
-          <div className="comedy">
-            <h3>Comedy</h3>
-            <ul>
-              {allMovies &&
-                (getTopFive('Comedy').length ? (
-                  getTopFive('Comedy')
-                ) : (
-                  <li>No Comedies At This Time</li>
-                ))}
-            </ul>
-          </div>
-          <div className="romance">
-            <h3>Romance</h3>
-            <ul>
-              {allMovies &&
-                (getTopFive('Romance').length ? (
-                  getTopFive('Romance')
-                ) : (
-                  <li>No Romance At This Time</li>
-                ))}
-            </ul>
-          </div>
-          <div className="action">
-            <h3>Action</h3>
-            <ul>
-              {allMovies &&
-                (getTopFive('Action').length ? (
-                  getTopFive('Action')
-                ) : (
-                  <li>No Actions At This Time</li>
-                ))}
-            </ul>
-          </div>
-          <div className="horror">
-            <h3>Horror</h3>
-            <ul>
-              {allMovies &&
-                (getTopFive('Horror').length ? (
-                  getTopFive('Horror')
-                ) : (
-                  <li>No Horrors At This Time</li>
-                ))}
-            </ul>
-          </div>
-          <div className="drama">
-            <h3>Drama</h3>
-            <ul>
-              {allMovies &&
-                (getTopFive('Drama').length ? (
-                  getTopFive('Drama')
-                ) : (
-                  <li>No Dramas At This Time</li>
-                ))}
-            </ul>
-          </div>
-        </div>
-        <div className="sideNote">
-          <h2>Aside</h2>
-        </div>
-      </main>
-    </>
-  );
+   return (
+      <>
+         <main className='homeMain'>
+            <div className='homeList'>
+               <div className='comedy'>
+                  <ul>
+                     <h3>COMEDY</h3>
+                     {allMovies &&
+                        (getTopFive('Comedy').length ? (
+                           getTopFive('Comedy')
+                        ) : (
+                           <li>No Comedies At This Time</li>
+                        ))}
+                  </ul>
+               </div>
+               <div className='romance'>
+                  <ul>
+                     <h3>Romance</h3>
+                     {allMovies &&
+                        (getTopFive('Romance').length ? (
+                           getTopFive('Romance')
+                        ) : (
+                           <li>No Romance At This Time</li>
+                        ))}
+                  </ul>
+               </div>
+               <div className='action'>
+                  <ul>
+                     <h3>Action</h3>
+                     {allMovies &&
+                        (getTopFive('Action').length ? (
+                           getTopFive('Action')
+                        ) : (
+                           <li>No Actions At This Time</li>
+                        ))}
+                  </ul>
+               </div>
+               <div className='horror'>
+                  <ul>
+                     <h3>Horror</h3>
+                     {allMovies &&
+                        (getTopFive('Horror').length ? (
+                           getTopFive('Horror')
+                        ) : (
+                           <li>No Horrors At This Time</li>
+                        ))}
+                  </ul>
+               </div>
+               <div className='drama'>
+                  <ul>
+                     <h3>Drama</h3>
+                     {allMovies &&
+                        (getTopFive('Drama').length ? (
+                           getTopFive('Drama')
+                        ) : (
+                           <li>No Dramas At This Time</li>
+                        ))}
+                  </ul>
+               </div>
+            </div>
+            <div className='sideNote'>
+               <h2>Aside</h2>
+            </div>
+         </main>
+      </>
+   );
 };
 
 export default Home;
