db66ea6f4fe9f3fb17b97cfb0e4ebf77304a449c
pjl4 <pj.lutz17@gmail.com>
2020-02-19 12:30:57 -0500

    Updates

---
+++ precache-manifest.8ea8ffa92cf40b97f04a5138e86c78e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a334cd7d8db97d4f9002cdb5bc07365f",
+    "url": "/moviesFrontend/index.html"
+  },
+  {
+    "revision": "72d9ba95dc9f6c586056",
+    "url": "/moviesFrontend/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "952c480da464edc00e24",
+    "url": "/moviesFrontend/static/css/main.fa816fbc.chunk.css"
+  },
+  {
+    "revision": "72d9ba95dc9f6c586056",
+    "url": "/moviesFrontend/static/js/2.7563421f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/moviesFrontend/static/js/2.7563421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952c480da464edc00e24",
+    "url": "/moviesFrontend/static/js/main.d58a9ced.chunk.js"
+  },
+  {
+    "revision": "30604171bd805a4e1792",
+    "url": "/moviesFrontend/static/js/runtime-main.2be68c7b.js"
+  }
+]);
