cf66e1beafc02604703e8b66693886eeac199046
mmarmol88 <mindysoto88@gmail.com>
2020-02-20 15:15:54 -0500

    ran test for Suzann

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Container';
@@ -9,53 +9,51 @@ import './HeaderContainer.css';
 
 const HeaderContainer = props => {
   return (
-     <div>
-        <Navbar className='nav-bar'>
-           <img className='logo' src={logo} alt='logo' />
-           <Container></Container>
-           <Container>
-              <div classname='headRow'></div>
-              <Link to='/'>
-                 <h3 className='homeHead'>Home </h3>
+    <div>
+      <Navbar className="nav-bar">
+        <img className="logo" src={logo} alt="logo" />
+        <Container></Container>
+        <Container>
+          <div className="headRow"></div>
+          <Link to="/">
+            <h3 className="homeHead">Home </h3>
+          </Link>
+          {localStorage.id && (
+            <Link to="/new">
+              <h3 className="submitHead">Submit</h3>
+            </Link>
+          )}
+
+          {!localStorage.id && (
+            <Link to="/login">
+              <h3 className="loginHead">Log in</h3>
+            </Link>
+          )}
+
+          {localStorage.id && (
+            <h3 className="hiHead">
+              Hi,{' '}
+              <Link to={`/user/${localStorage.id}`}>
+                {localStorage.username}
               </Link>
-              {localStorage.id && (
-                 <Link to='/new'>
-                    <h3 className='submitHead'>Submit</h3>
-                 </Link>
-              )}
-
-              {!localStorage.id && (
-                 <Link to='/login'>
-                    <h3 className='loginHead'>Log in</h3>
-                 </Link>
-              )}
-
-              {localStorage.id && (
-                 <h3 className='hiHead'>
-                    Hi,{' '}
-                    <Link to={`/user/${localStorage.id}`}>
-                       {localStorage.username}
-                    </Link>
-                    !
-                 </h3>
-              )}
-              {!localStorage.id && (
-                 <div></div>
-              )}
-              {localStorage.username && (
-                 <Button
-                    href='/moviesFrontend'
-                    onClick={() => {
-                       localStorage.clear();
-                    }}
-                    variant='warning'
-                 >
-                    Logout
-                 </Button>
-              )}
-           </Container>
-        </Navbar>
-     </div>
+              !
+            </h3>
+          )}
+          {!localStorage.id && <div></div>}
+          {localStorage.username && (
+            <Button
+              href="/moviesFrontend"
+              onClick={() => {
+                localStorage.clear();
+              }}
+              variant="warning"
+            >
+              Logout
+            </Button>
+          )}
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
